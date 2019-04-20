186ad19e5caf17b0c2a9742b819106cbb391ec65
wdk <2634647743@qq.com>
2019-04-20 15:53:10 +0800

    增加材质文件夹 修改装备时出现的bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,7 +50,7 @@ public class PlayerController : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
         moveController = GetComponent<CharacterController>();
         playerManager = GetComponent<PlayerManager>();
-        ItemManager.instance.nowWeapon.item = ItemManager.instance.cover.itemList[2];
+        ItemManager.instance.equip(new ItemBlock (ItemManager.instance.cover.itemList[1]));
     }
     void FixedUpdate()
     {
