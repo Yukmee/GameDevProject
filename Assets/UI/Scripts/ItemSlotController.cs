c85a7224dc27e9151f5126b7fe207abda6d681d1
Boyu Li <1251853265@qq.com>
2019-04-21 23:29:52 +0800

    Added RemoveBtn for Item Slots

--- Assets/UI/Scripts/ItemSlotController.cs
+++ Assets/UI/Scripts/ItemSlotController.cs
@@ -1,7 +1,5 @@
 ﻿using mygame;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class ItemSlotController : MonoBehaviour
@@ -14,24 +12,26 @@ public class ItemSlotController : MonoBehaviour
     private void Awake()
     {
         ItemNumber = GameObject.Find("ItemNumber");
+        _itemManager = ItemManager.instance;
     }
 
     
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
 
+        // TODO: - Delete me
         ItemManager.instance.weaponList.Add(new ItemBlock(ItemManager.instance.itemDictionary[0]));
-        
+
         // Get Index First
         var index = transform.GetSiblingIndex();
 
-        Debug.Log("Weapon List Count: " + ItemManager.instance.weaponList.Count);
+        // Debug.Log("Weapon List Count: " + ItemManager.instance.weaponList.Count);
         
         if (index < ItemManager.instance.weaponList.Count)
         {
             // TODO: - Delete me
-            Debug.Log(this + " / " + index);
+            // Debug.Log(this + " / " + index);
 
             _itemBlock = ItemManager.instance.weaponList[index];
         }
@@ -40,16 +40,27 @@ public class ItemSlotController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (_itemBlock != null)
         {
-            Debug.Log(_itemBlock.item);
-            Debug.Log(_itemBlock.num);
-            Debug.Log(ItemNumber);
-            Debug.Log(ItemNumber.GetComponent<Text>().text);
-            // ItemNumber.GetComponent<TextMeshPro>().text = _itemBlock.num.ToString();
+            // Debug.Log(_itemBlock.item);
+            // Debug.Log(_itemBlock.num);
+            // Debug.Log(ItemNumber);
+        
+            // TODO: - 展示对应的图片
+            
+            // Display Item Number
+            ItemNumber.GetComponent<Text>().text = _itemBlock.num.ToString();
         }
         
+        // Test RayCast
+        
+        
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("✅MouseOver!");
     }
 }
