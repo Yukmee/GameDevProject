d1fe53d371ccafe735f4199ae033bb86314e8dbf
Boyu Li <1251853265@qq.com>
2019-04-21 22:34:22 +0800

    Update ItemSlot Prefab

--- Assets/Scripts/Item/itemBlock.cs
+++ Assets/Scripts/Item/itemBlock.cs
@@ -12,11 +12,19 @@ namespace mygame
         public Item item;
         public int num;
         
+        // 快捷Init函数，默认数量为1
         public ItemBlock(Item item)
         {
             this.item = item;
+            num = 1;
         }
         
+        public ItemBlock(Item item, int num)
+        {
+            this.item = item;
+            this.num = num;
+        }
+
         public ItemBlock()
         {
             
--- Assets/UI/Scripts/ItemSlotController.cs
+++ Assets/UI/Scripts/ItemSlotController.cs
@@ -1,18 +1,18 @@
 ﻿using mygame;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
 
 public class ItemSlotController : MonoBehaviour
 {
     private ItemBlock _itemBlock;
     private GameObject ItemNumber;
-    
-    private GameObject WeaponTab;
+
     private ItemManager _itemManager;
 
     private void Awake()
     {
-        WeaponTab = GameObject.Find("WeaponTab");
         ItemNumber = GameObject.Find("ItemNumber");
     }
 
@@ -20,17 +20,18 @@ public class ItemSlotController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        ItemManager.instance.weaponList.Add(new ItemBlock(ItemManager.instance.itemDictionary[0]));
+        
         // Get Index First
         var index = transform.GetSiblingIndex();
 
-        Debug.Log(ItemManager.instance.weaponList.Count);
+        Debug.Log("Weapon List Count: " + ItemManager.instance.weaponList.Count);
         
         if (index < ItemManager.instance.weaponList.Count)
         {
-
             // TODO: - Delete me
             Debug.Log(this + " / " + index);
-
 
             _itemBlock = ItemManager.instance.weaponList[index];
         }
@@ -43,7 +44,11 @@ public class ItemSlotController : MonoBehaviour
     {
         if (_itemBlock != null)
         {
-            ItemNumber.GetComponent<TextMeshPro>().text = _itemBlock.num.ToString();
+            Debug.Log(_itemBlock.item);
+            Debug.Log(_itemBlock.num);
+            Debug.Log(ItemNumber);
+            Debug.Log(ItemNumber.GetComponent<Text>().text);
+            // ItemNumber.GetComponent<TextMeshPro>().text = _itemBlock.num.ToString();
         }
         
     }
